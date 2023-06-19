d977725279917fab09292c0460cff81b132afae5
Kazuhiro Homma <kazu.homma@gmail.com>
2023-06-20 01:22:51 +0900

    oretter 4bit

--- ore-tweets-lora/inference.py
+++ ore-tweets-lora/inference.py
@@ -1,16 +1,25 @@
 import torch
 from peft import PeftModel, PeftConfig
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 model_name = "rinna/japanese-gpt-neox-3.6b-instruction-ppo"
 peft_name = "lorappo-rinna-3.6b"
 output_dir = "lorappo-rinna-3.6b-results"
 
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
 # モデルの準備
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    load_in_8bit=True,
-    device_map="auto",
+    quantization_config=bnb_config, 
+    device_map={"":0},
+    # load_in_8bit=True,
+    # device_map="auto",
 )
 
 # トークナイザーの準備
@@ -87,7 +96,9 @@ def generate(instruction, input=None, maxTokens=256) -> str:
         return 'Warning: no <eos> detected ignoring output'
 
 
-# テキスト生成
-print("自然言語処理ってさ、\n{0}".format(generate('自然言語処理ってさ、')))
-print("台風近づいてるなぁ。\n{0}".format(generate('台風近づいてるなぁ。')))
-
+# ユーザーからの入力を受け付ける
+import readline
+while True:
+    msg = input("ユーザー: ")
+    msg = msg.replace("\n", "")
+    print("{0}\n{1}".format(msg, generate(msg).replace("\n", "")))
