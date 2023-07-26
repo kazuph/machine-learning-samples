2b6bf43484d14a573bbfd20dc8d1c0ab68574e03
Kazuhiro Homma <kazu.homma@gmail.com>
2023-07-26 10:06:53 +0900

    分割（途中）

--- whisper-speaker-diarization-space/app.py
+++ whisper-speaker-diarization-space/app.py
@@ -1,9 +1,9 @@
 # import whisper
 import psutil
-from transformers import pipeline
 import contextlib
 import wave
 from gpuinfo import GPUInfo
+import demucs.separate
 from pyannote.core import Segment
 from pyannote.audio import Audio
 from pyannote.audio.pipelines.speaker_verification import PretrainedSpeakerEmbedding
@@ -14,16 +14,16 @@ import pandas as pd
 import time
 import os
 import base64
-import random
 import subprocess
 import shutil
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_score
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 import yt_dlp
 import torch
-import pyannote.audio
 
 from pyannote.audio import Pipeline
 from pydub import AudioSegment
@@ -31,105 +31,100 @@ from pydub import AudioSegment
 whisper_models = ["tiny", "base", "small", "medium", "large-v1", "large-v2"]
 source_languages = {
     "en": "English",
-    "zh": "Chinese",
     "ja": "Japanese",
-    # "de": "German", "es": "Spanish", "ru": "Russian", "ko": "Korean", "fr": "French", "pt": "Portuguese", "tr": "Turkish", "pl": "Polish", "ca": "Catalan", "nl": "Dutch", "ar": "Arabic", "sv": "Swedish", "it": "Italian", "id": "Indonesian", "hi": "Hindi", "fi": "Finnish", "vi": "Vietnamese", "he": "Hebrew", "uk": "Ukrainian", "el": "Greek", "ms": "Malay", "cs": "Czech", "ro": "Romanian", "da": "Danish", "hu": "Hungarian", "ta": "Tamil", "no": "Norwegian", "th": "Thai", "ur": "Urdu", "hr": "Croatian", "bg": "Bulgarian", "lt": "Lithuanian", "la": "Latin", "mi": "Maori", "ml": "Malayalam", "cy": "Welsh", "sk": "Slovak", "te": "Telugu", "fa": "Persian", "lv": "Latvian", "bn": "Bengali", "sr": "Serbian", "az": "Azerbaijani", "sl": "Slovenian", "kn": "Kannada", "et": "Estonian", "mk": "Macedonian", "br": "Breton", "eu": "Basque", "is": "Icelandic", "hy": "Armenian", "ne": "Nepali", "mn": "Mongolian", "bs": "Bosnian", "kk": "Kazakh", "sq": "Albanian", "sw": "Swahili", "gl": "Galician", "mr": "Marathi", "pa": "Punjabi", "si": "Sinhala", "km": "Khmer", "sn": "Shona", "yo": "Yoruba", "so": "Somali", "af": "Afrikaans", "oc": "Occitan", "ka": "Georgian", "be": "Belarusian", "tg": "Tajik", "sd": "Sindhi", "gu": "Gujarati", "am": "Amharic", "yi": "Yiddish", "lo": "Lao", "uz": "Uzbek", "fo": "Faroese", "ht": "Haitian creole", "ps": "Pashto", "tk": "Turkmen", "nn": "Nynorsk", "mt": "Maltese", "sa": "Sanskrit", "lb": "Luxembourgish", "my": "Myanmar", "bo": "Tibetan", "tl": "Tagalog", "mg": "Malagasy", "as": "Assamese", "tt": "Tatar", "haw": "Hawaiian", "ln": "Lingala", "ha": "Hausa", "ba": "Bashkir", "jw": "Javanese", "su": "Sundanese",
 }
 
 source_language_list = [key[0] for key in source_languages.items()]
-
-# # MODEL_NAME = "vumichien/whisper-medium-jp"
-MODEL_NAME = "vumichien/whisper-large-v2-mix-jp"
-# lang = "ja"
-#
-# device = 0 if torch.cuda.is_available() else "cpu"
-# pipe = pipeline(
-#     task="automatic-speech-recognition",
-#     model=MODEL_NAME,
-#     chunk_length_s=30,
-#     device=device,
-# )
-# pipe.model.config.forced_decoder_ids = pipe.tokenizer.get_decoder_prompt_ids(
-#     language=lang, task="transcribe")
-
-
-def transcribe(microphone, file_upload):
-    warn_output = ""
-    if (microphone is not None) and (file_upload is not None):
-        warn_output = (
-            "WARNING: You've uploaded an audio file and used the microphone. "
-            "The recorded file from the microphone will be used and the uploaded audio will be discarded.\n"
-        )
-
-    elif (microphone is None) and (file_upload is None):
-        return "ERROR: You have to either use the microphone or upload an audio file"
-
-    file = microphone if microphone is not None else file_upload
-
-    text = pipe(file)["text"]
-
-    return warn_output + text
-
-
-def _return_yt_html_embed(yt_url):
-    video_id = yt_url.split("?v=")[-1]
-    HTML_str = (
-        f'<center> <iframe width="500" height="320" src="https://www.youtube.com/embed/{video_id}"> </iframe>'
-        " </center>"
-    )
-    return HTML_str
-
-
-def yt_transcribe(yt_url):
-
-    ydl_opts = {
-        'format': 'bestvideo*+bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': 'audio.%(ext)s',
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([yt_url])
-
-    text = pipe("audio.mp3")["text"]
-    return html_embed_str, text
 
 
 def convert_time(secs):
     return datetime.timedelta(seconds=round(secs))
 
 
+# def get_youtube(video_url):
+#     ydl_opts = {
+#         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+#     }
+#
+#     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#         info = ydl.extract_info(video_url, download=False)
+#         abs_video_path = ydl.prepare_filename(info)
+#         ydl.process_info(info)
+#
+#     print("Success download video")
+#     print(abs_video_path)
+#
+#     return abs_video_path
+
 def get_youtube(video_url):
     ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(video_url, download=False)
-        abs_video_path = ydl.prepare_filename(info)
+        abs_audio_path = ydl.prepare_filename(info).rsplit(".", 1)[0] + ".mp3"
         ydl.process_info(info)
 
-    print("Success download video")
-    print(abs_video_path)
-    return abs_video_path
+    print("Success download audio")
+    print(abs_audio_path)
+
+    return abs_audio_path
 
 
-def speech_to_text(audio_file_path, video_file_path, selected_source_lang, whisper_model, num_speakers, num_cut_time, is_pyannote):
+def separate_audio(input_path):
+    print(f"input_path: {input_path}")
+    demucs.separate.main(
+        ["--two-stems", "vocals", "-n", "htdemucs_ft", input_path, "-o", "downloads"])
+
+    # downloadsに作成されたディレクトリの中で最も更新時間が新しいものを取得
+    dirs = os.listdir("downloads/htdemucs_ft")
+    dirs.sort(key=lambda x: os.path.getmtime("downloads/htdemucs_ft/" + x))
+    latest_dir = dirs[-1]
+    print(f"latest_dir: {latest_dir}")
+
+    return f"/app/downloads/htdemucs_ft/{latest_dir}/vocals.wav"
+
+
+def cut_audio(input_path):
+    sound = AudioSegment.from_wav(input_path)
+
+    chunks = split_on_silence(sound,
+                              min_silence_len=600,  # 無音と判断する最小の長さ（ミリ秒）
+                              silence_thresh=-40     # 無音と判断する閾値（dB）
+                              )
+
+    # 分割したチャンクを結合する
+    output = chunks[0]
+    for chunk in chunks[1:]:
+        output += chunk
+
+    # 結果を保存する
+
+    # downloadsに作成されたディレクトリの中で最も更新時間が新しいものを取得
+    dirs = os.listdir("downloads/htdemucs_ft")
+    dirs.sort(key=lambda x: os.path.getmtime("downloads/htdemucs_ft/" + x))
+    latest_dir = dirs[-1]
+    print(f"latest_dir: {latest_dir}")
+    output.export(
+        f"/app/downloads/htdemucs_ft/{latest_dir}/vocals_cut.wav", format="wav")
+
+    return f"/app/downloads/htdemucs_ft/{latest_dir}/vocals_cut.wav"
+
+
+def speech_to_text(audio_file_path, selected_source_lang, whisper_model, num_speakers, num_cut_time):
 
     # output/以下は一度空にする
     if os.path.exists("output"):
         shutil.rmtree("output")
     os.makedirs('output', exist_ok=True)
 
-    if is_pyannote:
-        AUTH_TOKEN = os.environ.get("HUGGINGFACE_AUTH_TOKEN", None)
-        diarization_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
-                                                        use_auth_token=AUTH_TOKEN)
-        diarization_pipeline.to(torch.device('cuda'))
     """
     # Transcribe youtube link using OpenAI Whisper
     1. Using Open AI's Whisper model to seperate audio into segments and generate transcripts.
@@ -146,12 +141,11 @@ def speech_to_text(audio_file_path, video_file_path, selected_source_lang, whisp
     # model = WhisperModel(whisper_model, compute_type="int8")
     time_start = time.time()
 
-    print("video_file_path: ", video_file_path)
     print("audio_file_path: ", audio_file_path)
-    if (video_file_path == None and audio_file_path == None):
+    if (audio_file_path == None):
         raise ValueError("Error no input")
 
-    file_path = video_file_path or audio_file_path
+    file_path = audio_file_path
     print(file_path)
     # Read and convert youtube video
     _, file_ending = os.path.splitext(f'{file_path}')
@@ -182,57 +176,21 @@ def speech_to_text(audio_file_path, video_file_path, selected_source_lang, whisp
 
         segments = []
 
-        if is_pyannote:
-            diarization = diarization_pipeline(audio_file_path)
-            for turn, _, speaker in diarization.itertracks(yield_label=True):
-                chunk = {}
-                chunk["start"] = turn.start
-                chunk["end"] = turn.end
+        segments_raw, info = model.transcribe(
+            audio_file_path, **transcribe_options)
 
-                # startからendまでの音声を切り出す
-                # stdoutは表示しない
-                dir_name = f"output/{speaker}"
-                os.makedirs(dir_name, exist_ok=True)
+        # Convert back to original openai format
+        for segment_chunk in segments_raw:
+            chunk = {}
+            chunk["start"] = segment_chunk.start
+            chunk["end"] = segment_chunk.end
+            chunk["text"] = segment_chunk.text
 
-                file_name = f"{turn.start:04.1f}_{turn.end:04.1f}_{(turn.end - turn.start):02.1f}.wav"
-                subprocess.run(
-                    f"ffmpeg -y -i {audio_file_path} -ss {turn.start} -to {turn.end} -ar 16000 -ac 1 -c:a pcm_s16le {dir_name}/{file_name}", shell=True, stdout=subprocess.DEVNULL)
+            segments.append(chunk)
+            print(
+                f"start={chunk['start']:.2f}s end={chunk['end']:.2f}s text={chunk['text']}")
 
-                segments_raw, info = model.transcribe(
-                    f"{dir_name}/{file_name}", **transcribe_options)
-
-                text = ""
-                for segment_chunk in segments_raw:
-                    text += segment_chunk.text
-                    print(
-                        f"✘ start={segment_chunk.start:.1f}s end={segment_chunk.end:.1f}s speaker={speaker}")
-                chunk["text"] = text
-                chunk["speaker"] = speaker
-
-                # wavをbase64に変換して保存
-                chunk["audio"] = "data:audio/wav;base64," + base64.b64encode(
-                    open(f"{dir_name}/{file_name}", "rb").read()).decode("utf-8")
-                segments.append(chunk)
-                print(
-                    f"start={turn.start:.2f}s end={turn.end:.2f}s speaker_{speaker}, text={chunk['text']}")
-
-            print("transcribe audio done with pyannote")
-        else:
-            segments_raw, info = model.transcribe(
-                audio_file_path, **transcribe_options)
-
-            # Convert back to original openai format
-            for segment_chunk in segments_raw:
-                chunk = {}
-                chunk["start"] = segment_chunk.start
-                chunk["end"] = segment_chunk.end
-                chunk["text"] = segment_chunk.text
-
-                segments.append(chunk)
-                print(
-                    f"start={chunk['start']:.2f}s end={chunk['end']:.2f}s text={chunk['text']}")
-
-            print("transcribe audio done with fast whisper")
+        print("transcribe audio done with fast whisper")
     except Exception as e:
         raise RuntimeError("Error converting video to audio")
 
@@ -320,22 +278,6 @@ def speech_to_text(audio_file_path, video_file_path, selected_source_lang, whisp
             objects['Audio'].append(
                 f"<audio controls src='{segment['audio']}' ></audio>")
 
-            # if i == 0 or segments[i - 1]["speaker"] != segment["speaker"]:
-            #     objects['Start'].append(str(convert_time(segment["start"])))
-            #     objects['Speaker'].append(segment["speaker"])
-            #     # audioタグを追加
-            #     objects['Audio'].append(
-            #         f"<audio controls src='{segment['audio']}' ></audio>")
-            #     if i != 0:
-            #         objects['End'].append(
-            #             str(convert_time(segments[i - 1]["end"])))
-            #         objects['Text'].append(text)
-            #         text = ''
-            # text += segment["text"] + ' '
-
-        # objects['End'].append(str(convert_time(segments[i - 1]["end"])))
-        # objects['Text'].append(text)
-
         time_end = time.time()
         time_diff = time_end - time_start
         memory = psutil.virtual_memory()
@@ -367,6 +309,7 @@ def speech_to_text(audio_file_path, video_file_path, selected_source_lang, whisp
             filenames.sort()
             for filename in filenames:
                 if filename.endswith(".wav"):
+                    print(f"filename: {filename}")
                     # wavファイルを見つけたら、それをAudioSegmentオブジェクトに読み込みます
                     audio = AudioSegment.from_wav(
                         os.path.join(output_dir, filename))
@@ -391,9 +334,13 @@ def speech_to_text(audio_file_path, video_file_path, selected_source_lang, whisp
 
 # ---- Gradio Layout -----
 # Inspiration from https://huggingface.co/spaces/RASMUS/Whisper-youtube-crosslingual-subtitles
-audio_in = gr.Audio(label="Audio file", mirror_webcam=False, type="filepath")
-video_in = gr.Video(label="Video file", mirror_webcam=False)
 youtube_url_in = gr.Textbox(label="Youtube url", lines=1, interactive=True)
+audio_in = gr.Audio(label="Audio file", mirror_webcam=False, type="filepath")
+separated_audio_in = gr.Audio(
+    label="音源分離済みAudio file", mirror_webcam=False, type="filepath")
+cut_audio_in = gr.Audio(
+    label="無音カット済みAudio file", mirror_webcam=False, type="filepath")
+
 df_init = pd.DataFrame(columns=['Start', 'End', 'Speaker', 'Text'])
 memory = psutil.virtual_memory()
 selected_source_lang = gr.Dropdown(choices=source_language_list, type="value",
@@ -404,7 +351,6 @@ number_speakers = gr.Number(
     precision=0, value=0, label="Input number of speakers for better results. If value=0, model will automatic find the best number of speakers", interactive=True)
 number_cut_time = gr.Number(
     precision=0, value=0, label="Input number of audio cut time. If value=0, not cut audio", interactive=True)
-is_pyannote = gr.Checkbox(label="Use Pyannote", value=False, interactive=True)
 system_info = gr.Markdown(
     f"*Memory: {memory.total / (1024 * 1024 * 1024):.2f}GB, used: {memory.percent}%, available: {memory.available / (1024 * 1024 * 1024):.2f}GB*")
 download_transcript = gr.File(label="Download transcript")
@@ -448,13 +394,23 @@ with demo:
                 youtube_url_in.render()
                 download_youtube_btn = gr.Button("Download Youtube video")
                 download_youtube_btn.click(get_youtube, [youtube_url_in], [
-                    video_in])
-                print(video_in)
+                    audio_in])
 
         with gr.Row():
             with gr.Column():
-                video_in.render()
                 audio_in.render()
+                separate_btn = gr.Button("Separate audio")
+                separate_btn.click(separate_audio, [audio_in], [
+                                   separated_audio_in])
+        with gr.Row():
+            with gr.Column():
+                separated_audio_in.render()
+                cut_btn = gr.Button("cut audio")
+                cut_btn.click(cut_audio, [separated_audio_in], [
+                    cut_audio_in])
+        with gr.Row():
+            with gr.Column():
+                cut_audio_in.render()
                 with gr.Column():
                     gr.Markdown('''
                     ##### Here you can start the transcription process.
@@ -465,11 +421,10 @@ with demo:
                 selected_whisper_model.render()
                 number_speakers.render()
                 number_cut_time.render()
-                is_pyannote.render()
                 transcribe_btn = gr.Button("Transcribe audio and diarization")
                 transcribe_btn.click(speech_to_text,
-                                     [audio_in, video_in, selected_source_lang,
-                                         selected_whisper_model, number_speakers, number_cut_time, is_pyannote],
+                                     [cut_audio_in, selected_source_lang,
+                                         selected_whisper_model, number_speakers, number_cut_time],
                                      # [transcription_df, system_info,
                                      [transcription_html, system_info,
                                          download_transcript]
@@ -488,45 +443,4 @@ with demo:
                 system_info.render()
                 gr.Markdown('''<center><img src='https://visitor-badge.glitch.me/badge?page_id=WhisperDiarizationSpeakers' alt='visitor badge'><a href="https://opensource.org/licenses/Apache-2.0"><img src='https://img.shields.io/badge/License-Apache_2.0-blue.svg' alt='License: Apache 2.0'></center>''')
 
-    with gr.Tab("Whisper Transcribe Japanese Audio"):
-        gr.Markdown(f'''
-              <div>
-              <h1 style='text-align: center'>Whisper Transcribe Japanese Audio</h1>
-              </div>
-              Transcribe long-form microphone or audio inputs with the click of a button! The fine-tuned
-              checkpoint <a href='https://huggingface.co/{MODEL_NAME}' target='_blank'><b>{MODEL_NAME}</b></a> to transcribe audio files of arbitrary length.
-          ''')
-        microphone = gr.inputs.Audio(
-            source="microphone", type="filepath", optional=True)
-        upload = gr.inputs.Audio(
-            source="upload", type="filepath", optional=True)
-        transcribe_btn = gr.Button("Transcribe Audio")
-        text_output = gr.Textbox()
-        with gr.Row():
-            gr.Markdown('''
-                ### You can test by following examples:
-                ''')
-        examples = gr.Examples(examples=["sample1.wav",
-                                         "sample2.wav",
-                                         ],
-                               label="Examples", inputs=[upload])
-        transcribe_btn.click(
-            transcribe, [microphone, upload], outputs=text_output)
-
-    with gr.Tab("Whisper Transcribe Japanese YouTube"):
-        gr.Markdown(f'''
-              <div>
-              <h1 style='text-align: center'>Whisper Transcribe Japanese YouTube</h1>
-              </div>
-                Transcribe long-form YouTube videos with the click of a button! The fine-tuned checkpoint:
-                <a href='https://huggingface.co/{MODEL_NAME}' target='_blank'><b>{MODEL_NAME}</b></a> to transcribe audio files of arbitrary length.
-            ''')
-        youtube_link = gr.Textbox(
-            label="Youtube url", lines=1, interactive=True)
-        yt_transcribe_btn = gr.Button("Transcribe YouTube")
-        text_output2 = gr.Textbox()
-        html_output = gr.Markdown()
-        yt_transcribe_btn.click(yt_transcribe, [youtube_link], outputs=[
-                                html_output, text_output2])
-
-demo.launch(debug=True, server_port=7860, server_name="0.0.0.0")
+demo.queue().launch(debug=True, server_port=7860, server_name="0.0.0.0")
